5227a73eb5136da5dc16eab8d903f96cc4dc8faa
nate <nianate@yandex.ru>
2024-08-28 13:36:37 +0300

    updated csfr and cors origins settiings.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
 CORS_ALLOWED_ORIGINS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+CSRF_TRUSTED_ORIGINS = os.getenv('ALLOWED_HOSTS_CSRF', 'http://localhost http://127.0.0.1').split(' ')
 
 
 INSTALLED_APPS = [
