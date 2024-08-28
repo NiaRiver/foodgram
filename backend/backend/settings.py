e5fd68407c07fc5bd15480525227a708b3c0f112
nate <nianate@yandex.ru>
2024-08-28 13:02:44 +0300

    updated csfr and cors origins settiings.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,11 @@ DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
+CORS_ALLOWED_ORIGINS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+
+CSRF_TRUSTED_ORIGINS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
