245072c5078011bbe1aa7c1b2d95c4b1419483cc
nate <nianate@yandex.ru>
2024-09-14 21:31:45 +0300

    "Updated serializers.py and nginx.conf files"

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -52,7 +52,7 @@ class SubList(serializers.ModelSerializer):
     last_name = serializers.ReadOnlyField(source="subscribed_to.last_name")
     avatar = serializers.SerializerMethodField()
     email = serializers.ReadOnlyField(source="subscribed_to.email")
-    id = serializers.ReadOnlyField(source="subscribed_to.id")
+    id = serializers.ReadOnlyField(source="subscribed_to.c")
 
     class Meta:
         model = Subscription
